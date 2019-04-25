9e6466aa62bd1e3c94255ef901b8ec5d5800cb3d
CodingOni <37724661+CodingOni@users.noreply.github.com>
2019-04-25 17:20:40 -0600

    Picture Updates+

--- src/data/teachers.js
+++ src/data/teachers.js
@@ -8,33 +8,32 @@ const TeacherList = [
     live: "http://someurl.com"
   },
   {
-    name: "NodeStradamus",
+    name: "Four In a Row",
     bio: "'NodeStra' is a software engineer and philosopher trying to leave the world better than he found it. He codes for non-profits, eCommerce, and large-scale web apps.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/24/898316d49dcd721476a0a6ebfd6bef9d-full.png",
+    img_src: "https://cdn1.imggmi.com/uploads/2019/4/25/bdd363ba6bdd6126de8a6a83b5827d2f-full.png",
     id: "teach-2",
     code: "http://someurl.com", 
     live: "http://someurl.com"
   },
   {
-    name: "Geo 'Lo' Cation",
+    name: "PrecisionCutz",
     bio: "Geo is a JavaScript developer working on large-scale applications. He's also a teacher who strives to support students in removing all barriers to learning code.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/24/898316d49dcd721476a0a6ebfd6bef9d-full.png",
+    img_src: "https://cdn1.imggmi.com/uploads/2019/4/26/850be53d0fefe211f38e9f6ac6f2d224-full.png",
     id: "teach-3",
     code: "http://someurl.com", 
     live: "http://someurl.com"
   },
   {
-    name: "Ecma Scriptnstuff",
+    name: "Sherrie Restauraunt",
     bio: "Ecma found her passion for computers and programming over 15 years ago. She is excited to introduce people to the wonderful world of JavaScript.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/24/898316d49dcd721476a0a6ebfd6bef9d-full.png",
+    img_src: "https://cdn1.imggmi.com/uploads/2019/4/26/8319ea4000f9a4f05182721ea6e612b8-full.png",
     id: "teach-4",
-    code: "http://someurl.com", 
     live: "http://someurl.com"
   },
   {
-    name: "Jay Query",
+    name: "Calculator",
     bio: "Jay is a developer, author of CSS: The Missing Manual, JavaScript & jQuery: The Missing Manual, and web development teacher.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/24/898316d49dcd721476a0a6ebfd6bef9d-full.png",
+    img_src: "https://cdn1.imggmi.com/uploads/2019/4/26/018326593044cae92b3fe4bf433a349e-full.png",
     id: "teach-5",
     code: "http://someurl.com", 
     live: "http://someurl.com"
