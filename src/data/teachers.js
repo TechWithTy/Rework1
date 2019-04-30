efdb1f4094679bfabf83cb4b72939248dc7bbdbb
CodingOni <37724661+CodingOni@users.noreply.github.com>
2019-04-29 20:23:26 -0400

    Image not showing on mobile fix

--- src/data/teachers.js
+++ src/data/teachers.js
@@ -2,7 +2,7 @@ const TeacherList = [
   {
     name: "2uper",
     bio: "This site is a rework of a clients attempt at creating their website. The website looked like it was from the early 2,000's ran at a snail's pace and had a way to many pages, in fact, it is still up right now 2uper.com. I began with modernizing the website with a moving carousel and a shop button that grabs the attention of the user. Then I broke the pages down to two pages and a drop-down of shop classes. The less clicks the better. I also added a contact page so the client can be easily contacted and improved overall performance. The client is still gathering information for the store.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/26/77d9d5822de41b554a26256a534a591e-full.png",
+    img_src: "https://lh3.googleusercontent.com/OyHn-xrIwaJbXxBMxs2YKMmfUzIAsJh78btUxYQTU5LeUlpKLPWU09aQ9jKqXM0HBezQjV7LM3e5omYB5GxIfUOnfZgarRgONJhDhKcA3S4yvhL9dBZYmNsV3vkypAQi9jaU2we79UzMHGWZ6yqsTUoVj56jsVJpjkZnunVf45w9xjCeL1ytQGHTubLz5c1sEDl9zKSj44yvQ9L6Bo7BNgBeVZottofU1nfKnZiHe5PAhDQ6qVFDc5D6yZoa1naxVzuUEaBl-vAngrGipYk_aLXthXGd0Apq1jIWEy_wfQ_m99UsS6cJ7yu_aoG6VkcWXyM8zOeqboI88LVD3gBW6wg-8yjNMUU916n5l39bHxgEQwFuxeIr1Z-PYVaovB09_FBoATla5Mo_dCxViA5Pxc4ik5yOj0v4MPCJF1AwhTiXZA45qsKA1wtKdf2JKoUb0p3b3y3573WUum-kE5BAGJV6-5SeGydqGw3wiF_wLfdRy4pajy70PiMLk99RXO9r-9h4ZWbzuJvMjWsJUr7iBng1fQa3q3o3cjMju2t-fr4wn4V5gB_BEhHGIeToFYhbzqY4fYPIgYcT0fmTdde_OOrTU2uHJdixQ-H0Gn-ctb2DOh_MpncAv1MrERGvTs98KUze4g6P0GF7Dgq_X_7h72aJ49nIeA=w474-h315-no",
     id: "teach-6",
     code: "https://github.com/CodingOni/2uper", 
     live: "https://codingoni.github.io/2uper/",
@@ -12,7 +12,7 @@ const TeacherList = [
   {
     name: "PrecisionCutz",
     bio: "I noticed many local barbershops do no have websites in my area. So I decided to take advantage of this and create a simple website for new customers to see available barbers and easily book them. This site has a book me a button that would direct users to their Booksy profile to book an appointment from the first page customers can see the barbers experience and when they work. I also added a page that shows the pricing for each service with a brief description. Knowing this place is Hard to find I included a Google Map API that in two  clicks from the homepage gives you directions to the barbershop",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/26/850be53d0fefe211f38e9f6ac6f2d224-full.png",
+    img_src: "https://lh3.googleusercontent.com/fTtBK5Yk3W0dmAfKRNgQMESwpZ_6RrJraHowzyOBvlpKhgyuWkAiMYYN4arn1BZswL7DLZ8Fgy4u2cCEz831KptKggKV5GRpUkusfUd2VQuXakeFrwEYX-0dPM8rTVwLdmCcr840oa-b3GTh9Ll-s4-6p-PrvErZmQU2cZ5-FJZ-oFHxnlUGIKSYAJeBSkKUcQryYnKc4ZQR0zNYxc1XrmxOLEVujcM8dh4O8vaPsLTBBhrAtEp6Y8GcSXQGzolpIkm4ly1YCRvnD7ONwgLICKkv-eP43l69Z2CU-2VZjhYS4sFtwS-8qsK1JcOuy1shN-Bv8I_-fw-nhkMcb_5zy_LegqD71-EuzWnCmlfuKwUt8PugkJotd1HrYBhPcNqxfKgdLe3Vn0FyLJkE2vxoBS2uOhI-Oj7d7h35MtJpdr7-T1qQt-DG0mHflgZyL4p9Ak35SFz8Df1z6aPbonpko3ukHatFvs7yeqhqffW9nnpwcNCZKaQ3oVouRzAEMjVgcqSjR_UeFDORDD1UkRPY9Ea0j8PmSzlVuTQlGruK4v_8a3K1SEJGgfdfTBrfY_CzOJpG8Fkj8NCTfPNbuyOHi2IJYg3mkq-0szRrfflwBLP986A3RpCoWTmKiqiRoCMpmbC6uEbo9qsAg4yAUdba3NUJ75oB0w=w373-h248-no",
     id: "teach-3",
     code: "https://github.com/CodingOni/PrecisionCutz", 
     live: "https://codingoni.github.io/PrecisionCutz/",
@@ -22,7 +22,7 @@ const TeacherList = [
   {
     name: "Calculator",
     bio: "Wanting to further my JavaScript knowledge I created a basic calculator that performs equations. I also made the keys accessible to the visually impaired this was very important to me because my grandfather is actually blind.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/26/018326593044cae92b3fe4bf433a349e-full.png",
+    img_src: "https://lh3.googleusercontent.com/AkFFAuwuQDrrKEN9yTn69uVLOtKxyCk2qhmaa6hJ9XQ53W21yzz_3-fWc7I0XPaCXiOAeeFFtIiLEJvzTecCxeh0NholVmr9Pr_kBoFN-th5s84n5uBcd5dLAWBHSAtArufuMkLaTrUzgTmFV-TJsL6QCQ3J_UtyGtarHcGh2XdZN0FTiKg85B1opVYsAbH5sEWXuRMrxN0bV_Kxhq8Mg2ObtoAO_S-XhXlvJp0hMnJXIsAdk3Yk8TtA1coINx-nkYaylGfpvHPQLNSXODrtoyglPYLsPHFENDScdGOjpgwY37jpXhGDC2fHey4dkTQr2Gl_mhYj0GzNQfbk0HF6GFN8TXcY1zEq-nvsbeG8_8QwgokHOB6I6zVkeELtoBOwvkOWWbFGBTF63VRGpcmK6gc7WBGbUW5ma7t4LiUxu3ELdtJ4R6MwH1k8icjDJO2fRnxasaELwaJHiaMpQ9psrTMP4qrM1LouEvtZ5QfNYkpnx8DzYneRYmw1zCY5EntHntwfasae0aveV-tgzm8GP788R-9YGEPoGQKGIJbADYc1dOIK-qoh2oJ_z_Si_gF-BAP3HgJMrxEQ7KEa97lxYfSOdDXJSXmis-IoJ8dYUHSZjKaFlu_LhbEFmJ8QEbtDUMa7dZQJblXq6x1KRKHgNwSmSaHRpg=w373-h248-no",
     id: "teach-5",
     code: "https://github.com/CodingOni/bCalculator", 
     live: "https://codingoni.github.io/bCalculator/calc.html",
@@ -32,7 +32,7 @@ const TeacherList = [
   {
     name: "Catharsis Code Live" ,
     bio: "This is a rework of a clients massage bussiness. She also attempted to create this site on her own using a third-party site. This site was clunky and unresponsive, with too many pages. My first step was making this site mobile responsive. And instead of their being a separate page for each service I was able to fit everything onto one simple screen with nothing further than a hover and a click away.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/24/898316d49dcd721476a0a6ebfd6bef9d-full.png",
+    img_src: "https://lh3.googleusercontent.com/8AlnOvnLztpXVUH3FMZqdkB1Dg7LXUAxSINyI--DOj9Qvg8RGjxVXvGMFlROHRDTYq_iH7pdqOE9HUbU6A0udDqZ8Fj5xzerdbvasainIiq0hS5ByddECk-cLQR4VrbVlF-WZiH5vl5pSkcwlF7mhcJrAW2AmcJY4y8SSjxy0hbPJxvsL6ZASE15mW6lAbmukrnlVtnHN5ZNlBOw1kfK_n18tOCcngL08tEva3LbbT129TauXOOAGNptTph1aAloot68N9PDAy8CU4qU1fWXz_36NHvXXF-dpi7m7loV-vrqQjzpT8ZbwksaYQBIftDMNYc4mZ89Qon6j_hlAffcWFdhrXsMnmDBu7tPXnWWazJb9HrRk5HwgP09wN1GSCJBtqo2BNB4XVWyehW80M5m3UN8syQG5SF0wTAZwGRLnyx6FTM0H61J9Y6sP2Zbzf86yZqyGnFqYImwnDW4PVYGTaEjKaATD46FsHK2Fb0zKwzYnl3xt0rFFKc3YBzRqhO3gvDMVNMDtLjpOLiXtPelLGBPwzSLB2LudgrBBR1PTNfVOK0UXZ-Ylg4NxHZLfxxhtgjoJlnjazqkR5vTdS-au2ceoDGZSQEPh1pfdWWehBoVEMeZutZma-iZZlhmEctSnpy3BkNS6C1ZNjSt4aa6WJcp9BRNPQ=w350-h233-no",
     id: "teach-1",
     code: "https://github.com/CodingOni/Catharsis-LLC", 
     live: "https://mycatharsis.000webhostapp.com/CatharsisLLC/index.html",
@@ -40,8 +40,8 @@ const TeacherList = [
   },
   {
     name: "Four In a Row",
-    bio: "'I wanted to strengthen my JavaScript skills with another game so I created a Four In a row game. Each player is assigned a color and the first to get four coins in a row win the game. The game uses arrow keys to function correctly.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/25/bdd363ba6bdd6126de8a6a83b5827d2f-full.png",
+    bio: "I wanted to strengthen my JavaScript skills with another game so I created a Four In a row game. Each player is assigned a color and the first to get four coins in a row win the game. The game uses arrow keys to function correctly.",
+    img_src: "https://lh3.googleusercontent.com/WHFaD1ydNloQKCwnGX8x3jQcUoiVATeM4wcxK64keklmzrrxQxiHEK0O4rBOErGOIlv0HcteyoSZw7j6zEBmmK0sWUD2cMMPQHgUQauunuCnfRCmE65jg-y23eXKLoeTuj79Hjz7S6AVx7vGgC4b0W3nLeCBFmSNHQdCycbyxpC6cpUGvthLxCOdmsqFlpis6LxddIR-Bx8AVZnyf-i2-34gOb1yC9V8FzMzYR9rF6OpMlqumQOgTKHeE7bZLnObEocJo-DgMxxuIbfHJQS2k-vsaE8NoAAWiSg22AhfcmxpONE_65ukkOj7i3AHm00Gvmrmda0-gFukxEpSkFNAvnISKrxmHv2HT2sMls50jjgJcpaPxLri8oa7CdN47Rw_rCNDD-1ZoRI2r28j_kh_qtitl96XrZ2aNBBUM1Ln9qEQ-PsUDBGSnFFPmSIEtqJUaQzcd7Mk7fUvVH1MvlfwPVlTnGSnwGtB1d0xJr_qSiCyWRXcEcvDSI3BXqNF-Jf84F_rZnuQeCGDtlyO-0VnMndJJWLuNEOtPt6Ix8OQsDYMJvbk9-KAy-VWyjSe_8BWkB_ikwPTn4uhm2j6jVC9uXtbPTEcuN3DbnzTELkFO9BuvkZYbKuXSs5Z9eS3JPnFziGuP37BpXrmn2uN4KviimNObtyiDw=w373-h248-no",
     id: "teach-2",
     code: "https://github.com/CodingOni/FourInARowGame", 
     live: "https://codingoni.github.io/FourInARowGame/",
@@ -52,7 +52,7 @@ const TeacherList = [
   {
     name: "Sherrie Restauraunt",
     bio: "After some online browsing, i realized that one of the restaurants in my areas website was entirely outdated. And although her food had amazing reviews her site was the opposite. I used Wix.com to give her website an updated look feel and menu. I also kept her google maps API.",
-    img_src: "https://cdn1.imggmi.com/uploads/2019/4/26/8319ea4000f9a4f05182721ea6e612b8-full.png",
+    img_src: "https://lh3.googleusercontent.com/V7VFmiSojZrbFE5F8A9IsSsZ1eRwt3B5d_sFfUC-Eo8zdIhJU3mBLyDdRDY6zaL15TluzpJDihZZueTxnPqvT2ue0LHSBWhkj3BCLu4rnavvKmCWWaO-qKFpZ3DiK4SuqN31GRyvWW-XlwtwzUOYMqZ5tu-5s6gcyiF5YBdTeogN_3NEToB4lg1MroYpcKrWE31U1QXfbx4IcMDDhqBGBbZ-4VoShHJpr6dZzcS_t-eoYVxlqPsuA6Md4X0WIqBOEmHZZrbP0Q_Cxu07sdDIhzj6MW7OzuT8ibySgzgQZhxFao_Hzca2i_5GwXcPiTP_6rtZt34k9_ugPT4wufGY8Y4slB8UT_303bEo2lCaaPKNeeUWo6kmYQMTYmw5bD365_P4I3xudNM83fSfCewiR8T3KuHC4erNpyXJV7xAiXYJKHnyYrbGyIDwS56ej8AmPPtIj4l_7_6ilpF91AjRLyCKP3VzcguKggCHfxH23uaL2kxhYByEhfPvxCYfvHiK7TZRVRK02QjC1QVA49ySfHlIVP0XyAe3TRqIUHP-iDEBiwV0BFUwyzPXv6uGmtmAMwETm0s2AQDa5i8_Ybmbl_xXPut_RrstE-pj4N1nM357wrxxc1Cy9LAzlxWgWBCqZaDKLrwfxlGkTgyL5mhvP1f5EqGHxA=w373-h248-no",
     id: "teach-4",
     live: "https://tyriquedaniel14.wixsite.com/restaurant",
     used: "Wix, Google API"
